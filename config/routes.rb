ff5df528ec019a354993920ba20dc9e455c4e911
coolJenny <coolJenny009g@gmail.com>
2018-03-10 17:27:34 -0500

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :books
   resources :recipients
   resources :keywords
   resources :keywordgroups
 
-  devise_for :users, :controllers => { registrations: 'users/registrations', sessions: 'users/sessions' }
+  # devise_for :users, :controllers => { registrations: 'users/registrations', sessions: 'users/sessions' }
 
 	root to: 'statics#index'
 	get 'contact_us', to: 'statics#contact_us'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305071102) do
+ActiveRecord::Schema.define(version: 20180310221931) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
