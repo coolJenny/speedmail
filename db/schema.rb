5850e5a5fa248af62d1fc626403241a9d3a99f5f
coolJenny <coolJenny009g@gmail.com>
2018-03-11 19:20:19 -0400

    ...

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180311220046) do
+ActiveRecord::Schema.define(version: 20180311231945) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
