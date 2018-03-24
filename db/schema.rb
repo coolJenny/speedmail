bd74c4b8a9267f12adc096f9bc133ed08c672b1b
coolJenny <coolJenny009g@gmail.com>
2018-03-24 08:27:53 -0400

    update model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180311231945) do
+ActiveRecord::Schema.define(version: 20180324113033) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,15 @@ ActiveRecord::Schema.define(version: 20180311231945) do
     t.index ["keywordgroup_id"], name: "index_recipients_on_keywordgroup_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
+  end
+
   create_table "tests", force: :cascade do |t|
     t.string "name"
     t.boolean "order_state"
@@ -66,6 +75,8 @@ ActiveRecord::Schema.define(version: 20180311231945) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "provider", limit: 50, default: "", null: false
+    t.string "uid", limit: 500, default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
