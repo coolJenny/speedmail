dad584db9b383d21967c81886ae13b275f49a265
coolJenny <coolJenny009g@gmail.com>
2018-03-25 23:45:30 -0400

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations', sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth' }
   resources :users
+  resources :statics
   resources :books
   resources :recipients
   resources :keywords
